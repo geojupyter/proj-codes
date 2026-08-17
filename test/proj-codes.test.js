53e4714e70c666d2e0ef8241c8f8ca1836128b1d
Benjamin Szeghy <benjamin.szeghy@berkeley.edu>
2026-08-17 11:13:25 -0700

    Add tests (#4)
    Co-authored-by: Matt Fisher <3608264+mfisher87@users.noreply.github.com>
    Co-authored-by: Matt Fisher <mfisher87@gmail.com>

---
+++ test/proj-codes.test.js
@@ -0,0 +1,17 @@
+import assert from "node:assert/strict";
+import { test } from "node:test";
+
+import projCodes from "../index.js";
+
+test("generated artifact contains the expected number of CRS codes", () => {
+  assert.equal(Object.keys(projCodes).length, 6990);
+});
+
+test("EPSG:4326 matches expected", () => {
+  assert.deepEqual(projCodes["EPSG:4326"], {
+    auth_name: "EPSG",
+    code: "4326",
+    name: "WGS 84",
+    proj4string: "+proj=longlat +datum=WGS84 +no_defs +type=crs",
+  });
+});
